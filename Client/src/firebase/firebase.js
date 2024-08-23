e4f94019c3e67644f571dd987685c4d280358f21
Anshika <anshika1366@gmail.com>
2024-08-23 12:48:55 +0530

    final commits

--- Client/src/firebase/firebase.js
+++ Client/src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGUyd5gHjQt0mBIwBThj30amb41D-vNP8",
-  authDomain: "codepulse-279bb.firebaseapp.com",
-  projectId: "codepulse-279bb",
-  storageBucket: "codepulse-279bb.appspot.com",
-  messagingSenderId: "1053429833695",
-  appId: "1:1053429833695:web:5423ef00c6876f1277ae6a",
-  measurementId: "G-194HKXXGWP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
